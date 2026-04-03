10f2dd7cecbf540714149635f49c7dbe1bb9b52e
GabrielSal3s <gabrielcmara@gmail.com>
2026-04-03 00:24:12 -0300

    feat: adicionando headers de segurança

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ⚠️ Depois você pode remover isso (quando tiver tudo estável)
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: blob:;
+              font-src 'self';
+              connect-src 'self' https:;
+              frame-ancestors 'none';
+            `.replace(/\n/g, ""),
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ]
   },
 }
 
